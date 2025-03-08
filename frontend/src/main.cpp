2a2d8a7cb4c6b22fdc0cfd262cb5eedd06fbea68
Raj Pandya <rajpandya737@gmail.com>
2025-03-08 16:19:39 -0500

    update error messages and better parsing

--- frontend/src/main.cpp
+++ frontend/src/main.cpp
@@ -12,27 +12,34 @@ bool valid_python_tag(const std::deque<std::string>& lines) {
     // Rules for python tags, you cannot have python tags inside of each other
     // One must end before another is open
     // Every open tag must have a closing tag
-    // If these rules are not followed, the function will return false    bool in_python_block = false;
+    // If these rules are not followed, the function will return false
     bool in_python_block = false;
+    int line_number = 0;
+    int last_python_line = 0;
     for (const std::string& line : lines) {
         std::cout << line << std::endl;
-
+        line_number++;
         if (line.find("<python>") != std::string::npos) {
             if (in_python_block) {
+                std::cerr << "ERROR: Python tags cannot exist inside other Python tags, close the tag located at line " << last_python_line << " Before using more tags\n";
                 return false;
             }
             in_python_block = true;
+            last_python_line = line_number;
         }
 
         if (line.find("</python>") != std::string::npos) {
             if (!in_python_block) {
+                std::cerr << "ERROR: Closing tag on line " << last_python_line << " does not have an opening tag.\n";
                 return false;
             }
             in_python_block = false;
+            last_python_line = line_number;
         }
     }
 
     if (in_python_block) {
+        std::cerr << "ERROR: Closing tag not found for line " << last_python_line << ".\n";
         return false;
     }
 
