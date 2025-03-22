2d95112af2939d612d989bf86c172a54ed1b53f3
Raj Pandya <rajpandya737@gmail.com>
2025-03-22 10:08:44 -0400

    add new deps to pipeline and formating class

---
+++ PYTHML/include/utils.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+#include <tidy/tidy.h>
+#include <tidy/tidybuffio.h>
+#include <vector>
+
+const std::vector<std::string> formatHTML(const std::vector<std::string>&);
---
+++ PYTHML/src/utils.cpp
@@ -0,0 +1,5 @@
+#include "utils.hpp"
+
+const std::vector<std::string>& formatHTML(const std::vector<std::string>& inputHtml) {
+    
+}
